5a9cc820566b4a271e700027cd651a930ba4afd5
Admin <georgioji@gmail.com>
2026-02-04 16:21:22 -0700

    Added Pricing Table

--- core/views.py
+++ core/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from .models import InternetPlan
 
-
 # Create your views here.
 def home(request):
     pass
@@ -11,9 +10,12 @@ def pricing(request):
 
     formatted_plans = []
     for plan in plans:
+        # Convert any literal "\n" to actual newlines
+        description = plan.description.replace('\\n', '\n')
+
         formatted_plans.append({
             "title": plan.title,
-            "description": plan.description,
+            "description": description,
             "base_price": plan.base_price,
             "rental_price": plan.rental_price,
             "cta": plan.cta,
